c186428ad972ffc66404f32475fbb3ebe471df47
Smiley1144 <kovacsm519@gmail.com>
2024-10-03 20:19:56 +0200

    1003

---
+++ 00-lesson/canvas.js
@@ -0,0 +1,50 @@
+let canvas = document.getElementById('myCanvas');
+
+let ctx = canvas.getContext('2d');
+
+let image = document.querySelector('img');
+
+// Alakzatok
+
+// ctx.fillStyle = 'red';
+// ctx.fillRect(0, 0, 300, 200); // Téglalap készítés
+
+// ctx.fillStyle = 'blue';
+// ctx.beginPath();
+// ctx.arc(250, 125, 50, 0, Math.PI * 2); // Kör készítés
+// ctx.fill();
+
+// ctx.fillStyle = 'green';
+// ctx.beginPath();
+// ctx.arc(250, 125, 25, 0, Math.PI * 2);
+// ctx.fill();
+
+// Kép szerkesztés
+
+// ctx.drawImage(image, 100, 100) // A teljes kép beillesztése adott koordinátákra
+
+// ctx.drawImage(image, 110, 200, 100, 100); // A kép kicsinyített változatát illesszük be adott koordinátákra
+
+// ctx.drawImage(image, 300, 300, 200, 200, 350, 260, 350, 350); // Adott helyről, adott méretet, beilleszteni, meghatározott magasságban és szélességben
+
+// Oszlopdiagramm
+
+let data = [90, 340, 120, 260, 190, 300];
+
+function drawBarChart() {
+    let barWidth = 50;
+    let startX = 50;
+    let startY = canvas.height - 50;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for(let i = 0; i < data.length; i++){
+        let barHeight = data[i];
+        ctx.fillStyle = 'magenta';
+        ctx.fillRect(startX, startY - barHeight, barWidth, barHeight);
+        startX = startX + 100;
+    }
+}
+
+drawBarChart();
+
