13a374211703ea823f66b68d7a1a39279c1b258d
Smiley1144 <kovacsm519@gmail.com>
2024-10-10 20:18:01 +0200

    1010

--- 02-DOM/05-word-count/word_count.js
+++ 02-DOM/05-word-count/word_count.js
@@ -5,9 +5,31 @@ document.addEventListener('DOMContentLoaded', () => {
 //A táblázat új elemeinek létrehozásához használd: 
 //https://www.w3schools.com/jsref/met_document_createelement.asp
 
+// const countButton = document.getElementById('count-button');
+
+const resultTable = document.getElementById('result-table');
 
 
+document.getElementById('count-buttom').addEventListener('click', () =>{
+    const textArea = document.getElementById('text').value;
+    const words = textArea.split(' ');
+    let countObj = {};
+
+    if(textArea.innerText.length < 1){
+        alert('Write something first');
+    }
+
+    for(let i = 0; i < words.length; i++){
+        // if(){
+
+        // }
+    }
+
+    resultTable.innerHTML = '';
+    // remove.resultTable.innerHTML;
+})
 
 
-
+    
 });
+
