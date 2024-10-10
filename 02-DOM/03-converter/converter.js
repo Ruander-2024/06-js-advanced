ca181fc7f7e6a3ce98118539e4fcd8d7574795e4
Smiley1144 <kovacsm519@gmail.com>
2024-10-10 19:41:51 +0200

    03, 04 folder practice

--- 02-DOM/03-converter/converter.js
+++ 02-DOM/03-converter/converter.js
@@ -4,6 +4,36 @@ document.addEventListener('DOMContentLoaded',  () => {
     //Gombok nélkül is elkérheted az értékeket az oninput property segítségével
     //https://www.w3schools.com/jsref/event_oninput.asp
 
+    // const celsiusInput = document.getElementById('celsius-input-field');
+    // const fahrenheitInput = document.getElementById('fahrenheit-input-field');
+
+    // const celsiusToFahr = celsius => (celsius * 9/5) + 32;
+
+    document.querySelectorAll('input').forEach(input => input.addEventListener('change', (event) => {
+        const convert = event.target.value;
+        switch(event.target.id){
+            case 'celsius-input-field':
+                document.getElementById('fahrenheit-input-field').value = (convert * 9/5) + 32;
+                break;
+            case 'fahrenheit-input-field':
+                document.getElementById('celsius-input-field').value = (convert - 32) / 9 * 5;
+                break;
+        }
+        
+    }))
+
+    
+
 });
 
+
+// function celToFahr() {
+//     let celsius = document.getElementById('celsius-input-field').value;
+//     document.getElementById('fahrenheit-input-field').value = celsius * (9 / 5) + 32;
+// }
+
+// function fahrToCel() {
+//     let fahrenheit = document.getElementById('fahrenheit-input-field').value;
+//     document.getElementById('celsius-input-field').value = (fahrenheit - 32) / 9 * 5;
+// }
 
--- 02-DOM/04-todo-list/todo.js
+++ 02-DOM/04-todo-list/todo.js
@@ -5,5 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
     //Hint: Az új tagek (tr,td és button) létrehozásához használd:
     // https://www.w3schools.com/jsref/met_document_createelement.asp
 
+    document.getElementById('add-button').addEventListener('click', () => {
+        let addTr = document.createElement('tr'); // tr tag hozzáadása
+        let addTd = document.createElement('td'); // td tag hozzáadása
+
+        addTd.innerText = document.querySelector('#new-item-input-field').value;
+        addTr.appendChild(addTd);
+        
+        let removeBtn = document.createElement('button'); // törlő gomb hozzáadása
+        removeBtn.innerText = 'Remove'; // A gomb szövegének a beállítása
+        removeBtn.addEventListener('click', () => {
+            addTr.remove();
+        });
+
+        addTr.appendChild(removeBtn);
+        if(addTd.innerText.length > 0){
+            let table = document.querySelector('#items-table');
+            table.appendChild(addTr);
+        }
+        else{
+            alert("You can't add anything...")
+        }
+    })
+
     
 });
