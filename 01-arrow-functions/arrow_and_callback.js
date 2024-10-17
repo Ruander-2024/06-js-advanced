6bdebc5d62c6c40d56211ad02795a6442d709de3
Pavity Patrik <pavicpatrik@gmail.com>
2024-10-17 21:15:35 +0200

    10.17

--- 01-arrow-functions/arrow_and_callback.js
+++ 01-arrow-functions/arrow_and_callback.js
@@ -1,56 +1,76 @@
 
 const employees = [
-    {name: "Zachary Barron",age:  20,position: "manager",salary: 10000},
-    {name: "Carly Browne",age:  30,position: "kitchen porter",salary: 20000},
-    {name: "Hugo Clarke",age:  25,position: "bartender",salary: 25000},
-    {name: "Johhny Paine",age:  26,position: "bartender",salary: 17000},
-    {name: "Hillary Newman",age:  65,position: "manager",salary: 85000},
-    {name: "Bob",age:  50,position: "chef",salary: 65000},
-    {name: "Edith Smith",age:  45,position: "bartender",salary: 18000},
-    {name: "Savanna Millington",age:  41,position: "chef",salary: 43000}
+  {name: "Zachary Barron",age:  20,position: "manager",salary: 10000},
+  {name: "Carly Browne",age:  30,position: "kitchen porter",salary: 20000},
+  {name: "Hugo Clarke",age:  25,position: "bartender",salary: 25000},
+  {name: "Johhny Paine",age:  26,position: "bartender",salary: 17000},
+  {name: "Hillary Newman",age:  65,position: "manager",salary: 85000},
+  {name: "Bob",age:  50,position: "chef",salary: 65000},
+  {name: "Edith Smith",age:  45,position: "bartender",salary: 18000},
+  {name: "Savanna Millington",age:  41,position: "chef",salary: 43000}
 ];
 
 /**
- * @FELADAT
- * Készítsd el az alább metódusokat. Használj "arrow function"-ket, mint  callback function...
- * Ahol kell használj minél több beépített metódust.
- */
+* @FELADAT
+* Készítsd el az alább metódusokat. Használj "arrow function"-ket, mint  callback function...
+* Ahol kell használj minél több beépített metódust.
+*/
+/**
+* @TODO make a list of all the employees whose name starts with 'H'
+* Készíts egy listát az összes alkalmazottal, akinek a neve 'H'-val kezdődik
+*/
+
+  const employeeWithH = employees.filter(employee => employee.name[0] === 'H');
+
+  console.table(employeeWithH);
 
 /**
- * @TODO make a list of all the employees whose name starts with 'H'
- */
+* @TODO make a list of all the employees older than 30
+* Készíts egy listát a 30 évnél öregebbekről
+*/
+  const employeesOlder30 = employees.filter(employee => employee.age > 30);
 
-// console.table();
-
-/**
- * @TODO make a list of all the employees older than 30
- */
-
-// console.table();
+  console.table(employeesOlder30);
 
 /**
- * @TODO make a list of all the employees with salary greater than 30000, sorted by their salary (ascending)
- */
+* @TODO make a list of all the employees with salary greater than 30000, sorted by their salary (ascending)
+* Listázd ki az összes alkalmazottat, akinek a fizetése nagyobb mint 30000, szortírozd növekvő sorrendben
+*/
 
-// console.table();
+  const employeesGreaterSalary = employees.filter(employee => employee.salary > 30000).sort((a, b) => a.salary - b.salary);
 
-/**
- * @TODO make a list of all the employess with salary between 25000 and 65000 (inclusive on both ends)
- */
-
-//console.table();
+  console.table(employeesGreaterSalary);
 
 /**
- * @TODO make a list of all the bartenders' name
- */
+* @TODO make a list of all the employess with salary between 25000 and 65000 (inclusive on both ends)
+* Listázd ki az összes olyan alkalmazottat, akinek a fizetése 25000 és 65000 között van. (Beleértve mind a két végét)
+*/
 
-//console.table();
+  const employeesFilteredSalary = employees.filter(employee => employee.salary >= 25000 && employee.salary <= 65000);
+
+  console.table(employeesFilteredSalary);
 
 /**
- * @TODO calculate the average salary of the employees (use "reduce" method)
- */
+* @TODO make a list of all the bartender's name
+* Listázd ki az összes pincér nevét
+*/
 
-//console.log();
+  const bartendersName = employees.filter(employee => employee.position === 'bartender').map(bartender => bartender.name);
+
+  console.table(bartendersName);
+
+/**
+* @TODO calculate the average salary of the employees (use "reduce" method)
+* Számold ki az átlagfizetésüket az alkalmazottaknak
+*/
+
+  const avgSalary = employees.reduce((avgSalary, employee) => {
+      let salary = employee.salary;
+      avgSalary += salary;
+      return avgSalary;
+  }, 0) / employees.length;
+
+  console.log(avgSalary);
 
 /**
 * @TODO sum the number of letters of all the first names that are longer than 5 characters
@@ -63,9 +83,9 @@ const employees = [
 
 
 /**
- * @TODO make a list of all the monograms of employees who has both first and last names
- * example: {"Bob Smint", "John Jones", "Frank"} -> {"BS", "JJ"}
- * Write separatly the inner logics of your all arrow functions
- */
+* @TODO make a list of all the monograms of employees who has both first and last names
+* example: {"Bob Smint", "John Jones", "Frank"} -> {"BS", "JJ"}
+* Write separatly the inner logics of your all arrow functions
+*/
 
 //console.table();
