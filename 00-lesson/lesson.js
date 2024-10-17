cf6c066c3b47ba94d522be83c3722f5719a9c775
Peter-Kohlrusz <peter.kohlrusz@gmail.com>
2024-10-17 18:21:16 +0200

    10.17 filter indexOf

--- 00-lesson/lesson.js
+++ 00-lesson/lesson.js
@@ -75,3 +75,108 @@ book.title = 'Ez';
 
 console.log(book);
 console.log(book2);
+
+
+
+console.log();
+console.log();
+console.log();
+
+//Filter
+
+// const newArray = array.filter(callback(element, index, array));
+// callback - Arrow function
+// element - A tömb aktuális eleme
+// index - (opcionális) - Az aktuális elem indexe
+// array - (opcionális) - maga a tömb, amelyen lefut
+
+
+console.log('Filter metódus:');
+
+
+
+
+
+//2024.10.17
+
+const numbers = [1, 2, 3, 4, 5, 6];     //számok
+
+const evenNumbers = numbers.filter(number => number % 2 === 0);     //Páros számok
+
+console.log(evenNumbers);
+
+const people = [            // emberek
+    {name: 'John', age:25},
+    {name: 'Jane', age:17},
+    {name: 'Mike', age:21},
+    {name: 'Sarah', age:30},
+];
+
+const adults = people.filter(person => person.age >= 21);   //felnőttek 
+
+console.log(adults);
+
+const words = ['apple', 'banana', 'cat', 'dog', 'elephant' ];    // szavak
+
+const longWords = words.filter(words => words.length > 4);      // hosszú szavak
+
+console.log(longWords);
+
+const numbers2 = [1, 2, 3, 4, 3, 2, 5];
+
+const uniqueNumbers = 
+    numbers2.filter((value, index, self) => 
+        self.indexOf(value) === index);
+
+console.log(uniqueNumbers);
+
+console.log();
+console.log();
+console.log();
+
+// indexOf
+
+// Array.indexOf(searchElement, fromIndex);
+// searchElement - A keresett elem, amelynek az indexét szeretnénk megtalálni
+// fromIndex - (opcionális) Az index, ahonnan kezdődik a keresés, Alapértelmezetten 0
+
+console.log('indexOf metódus:');
+
+const numbers3 = [10, 20, 30, 40, 50, 30];
+
+const index = numbers3.indexOf(30);
+
+console.log(index);
+
+
+// function add(a, b){
+//         return a + b;
+// }
+
+const index2 = numbers3.indexOf(60);
+
+console.log(index2);
+
+const index3 = numbers3.indexOf(30, 3);
+
+console.log(index3);
+
+const index4 = numbers3.indexOf(30);
+
+console.log(index4);
+
+console.log();
+
+const string = 'Hello World';
+
+const srtIndex = string.indexOf('o');
+
+console.log(srtIndex);
+
+const strIndex2 = string.indexOf('O');  // Kis nagybetű érzékeny
+
+console.log(strIndex2);
+
+const strIndex3 = string.indexOf('ld');  // Hello World ben az ld előfordulása (9.karakter)
+
+console.log(strIndex3);
