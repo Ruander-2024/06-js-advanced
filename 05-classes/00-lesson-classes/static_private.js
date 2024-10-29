e811ffc66b95d9603c8e34d5ef786230dc435129
Smiley1144 <kovacsm519@gmail.com>
2024-10-29 20:35:46 +0100

    1029

---
+++ 05-classes/00-lesson-classes/static_private.js
@@ -0,0 +1,44 @@
+class Dog{
+    #sex; // private, nem tudjuk átírni és rejtve marad
+
+    constructor(name, age, sex){
+        this._name = name;
+        this._age = age;
+        this.#sex = sex;
+    }
+
+    get sex(){
+        return this.#sex; // Getter nélkül a private értéke rejtve marad undefined értéket kapunk
+    }
+
+    set sex(str){
+        this.#sex = str;
+    }
+
+    static logClassName(){
+        console.log('Dog');
+    }
+
+    static breed = 'basic';
+
+    
+    changeBreed(){
+        this.breed = 'not basic';
+        console.log(this.breed);
+    }
+    // A static breed értékét, nem tudom semmilyen úton kiiratni csak megváltoztatni
+    // changeBreed(){
+    //     console.log(this.breed);
+    // }
+}
+
+dog1 = new Dog('Pamacs', 4, 'male');
+console.log(dog1.sex);
+// dog1.sex = 'female'; // Megkapom az értékét, de megváltoztatni nem tudjuk getter-el
+dog1.sex = 'female';
+console.log(dog1.sex);
+dog2 = new Dog('Dogmeat', 6, 'female');
+
+console.log(dog1.logClassName); // A static-ot a class-on kívül nem éred el
+console.log(dog1.breed);
+dog1.changeBreed();
