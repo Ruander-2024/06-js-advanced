c9e006fec6389540a5c8adf59f28b200db46656c
Smiley1144 <kovacsm519@gmail.com>
2024-10-29 18:25:35 +0100

    constructor

---
+++ 05-classes/00-lesson-classes/constructor.js
@@ -0,0 +1,38 @@
+const books = [
+    {
+        title: 'Good Omens',
+        authors: ['Terry Pratchett', 'Neil Gaiman'],
+        rating: 4.25,
+        genres: ['fiction', 'fantasy'],
+        price: 20,
+      },
+      {
+        title: 'Bone: The Complete Edition',
+        authors: ['Jeff Smith'],
+        rating: 4.42,
+        genres: ['nonfiction', 'graphic novel', 'fantasy'],
+        price: 40,
+      },
+]
+const ages = 0
+
+class Animal{
+    constructor(name, numberOfLegs, age) {
+        this.name = name;
+        this.numberOfLegs = numberOfLegs;
+        this.age = age;
+    }
+
+    incrementAge(){
+        this.age = this.age + 1; // this.age++;     this.age += 2;
+    }
+}
+
+animal1 = new Animal('Buksi', 4, 6);
+console.log(animal1);
+console.log(animal1.numberOfLegs);
+animal1.incrementAge();
+console.log(animal1);
+
+animal2 = new Animal('Szimba', 5, 1);
+console.log(animal2);
