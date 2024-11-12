9ea13db38df0f7f4e5417ad37da0ccd6d38a2664
Csorgo-Peter <peter.csorgo@icloud.com>
2024-11-12 17:09:49 +0100

    11.08.

---
+++ 06-date/00-time-handling/time-handling.js
@@ -0,0 +1,37 @@
+// setTimeout
+
+console.log('Start');
+
+(function Timeout() {  // Várakoztathatjuk a lefutását egy programrésznek, így a lefutása a végén történik
+    setTimeout(() =>{
+        console.log('Eltelt 3 másodperc');
+        
+    }, 3000)
+})();
+
+console.log('End');
+
+// Interval - setInterval, clearInterval
+
+const timerText = document.getElementById('timer-text');
+const startButton = document.getElementById('start-btn');
+const pauseButton = document.getElementById('pause-btn');
+
+let counter = 0; // számláló
+let intervalID;
+
+startButton.addEventListener('click', () =>{
+    intervalID = setInterval(() => {
+        counter++;  // counter = counter + 1
+        timerText.innerHTML = counter;
+    }, 1000)
+
+    console.log(counter);
+    
+});
+
+pauseButton.addEventListener('click', () => {
+    clearInterval(intervalID);
+    console.log(counter);
+    
+})
